bb90565d5ab242a504f8dac840af62351de2222b
WhiteLady1 <megan15963@gmail.com>
2020-10-21 16:58:56 +0200

    finále všechno funguje

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 'use strict'
 
 let player = 'circle'
+const playDesc=document.querySelectorAll('button')
+let symbolPlayer=document.querySelector('.symbol')
 
 const activePayer=(event)=>{
-  event.target.classList.toggle('board__field--circle')
+    if (player==='circle') {
+      event.target.classList.add('board__field--circle')
+      event.target.disabled=true
+      player='cross'
+      symbolPlayer.src="images/cross.svg"
+      symbolPlayer.alt="Hraje křížek"
+    } else {
+      event.target.classList.add('board__field--cross')
+      event.target.disabled=true
+      player='circle'
+      symbolPlayer.src="images/circle.svg"
+      symbolPlayer.alt="Hraje kolečko"
+    }
 }
-document.querySelector('button').addEventListener('click', activePayer)
+
+for (let i = 0; i < playDesc.length; i++) {
+  playDesc[i].addEventListener('click', activePayer)
+}
