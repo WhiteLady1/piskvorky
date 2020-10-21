91ed0b1ab25189a883146d7d2b1828393849e75e
WhiteLady1 <megan15963@gmail.com>
2020-10-21 15:47:16 +0200

    První kolečko ve hře

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 
 let player = 'circle'
 
+const activePayer=(event)=>{
+  event.target.classList.toggle('board__field--circle')
+}
+document.querySelector('button').addEventListener('click', activePayer)
