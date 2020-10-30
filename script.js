e6c0cf09b74f08af4c22e5daddd68ba0b48a4b6a
WhiteLady1 <megan15963@gmail.com>
2020-10-30 17:46:13 +0100

    Prácena na poslední části - souřadnice kliknutí

--- script.js
+++ script.js
@@ -11,15 +11,35 @@ const activePayer=(event)=>{
       player='cross'
       symbolPlayer.src="images/cross.svg"
       symbolPlayer.alt="Hraje křížek"
+      console.log(getPosition(event.target));
     } else {
       event.target.classList.add('board__field--cross')
       event.target.disabled=true
       player='circle'
       symbolPlayer.src="images/circle.svg"
       symbolPlayer.alt="Hraje kolečko"
+      console.log(getPosition(event.target));
+
     }
 }
+//Doplnění na úkol č.5 => vrátí souřadnice kde jsem klikla
+const boardSize = 10 // 10x10
 
+const getPosition = (field) => {
+	let fieldIndex = 0
+	while (fieldIndex < playDesc.length) {
+		if (field === playDesc[fieldIndex]) {
+			break
+		}
+		fieldIndex++
+	}
+
+	return {
+		row: Math.floor(fieldIndex / boardSize),
+		column: fieldIndex % boardSize,
+	}
+}
+//
 for (let i = 0; i < playDesc.length; i++) {
   playDesc[i].addEventListener('click', activePayer)
 }
