7f829476bda1f3340d60fbaeb7db32f7ec1a4838
WhiteLady1 <megan15963@gmail.com>
2020-10-30 18:42:09 +0100

    řeším problém s getSymbol

--- script.js
+++ script.js
@@ -5,20 +5,27 @@ const playDesc=document.querySelectorAll('button')
 let symbolPlayer=document.querySelector('.symbol')
 
 const activePayer=(event)=>{
+  let pozice=getPosition(event.target)
+  let prvek=getField(pozice.row, pozice.column)
+  let symbolPrvku=getSymbol(prvek)
     if (player==='circle') {
       event.target.classList.add('board__field--circle')
       event.target.disabled=true
       player='cross'
       symbolPlayer.src="images/cross.svg"
       symbolPlayer.alt="Hraje křížek"
-      console.log(getPosition(event.target));
+      console.log(pozice);
+      console.log(prvek);
+      console.log(symbolPrvku);
     } else {
       event.target.classList.add('board__field--cross')
       event.target.disabled=true
       player='circle'
       symbolPlayer.src="images/circle.svg"
       symbolPlayer.alt="Hraje kolečko"
-      console.log(getPosition(event.target));
+      console.log(pozice);
+      console.log(prvek);
+      console.log(symbolPrvku);
 
     }
 }
@@ -37,6 +44,19 @@ const getPosition = (field) => {
 	return {
 		row: Math.floor(fieldIndex / boardSize),
 		column: fieldIndex % boardSize,
+	}
+}
+//
+//Pro číslo řádku a sloupce vrátí prvek
+const getField = (row, column) => playDesc[row * boardSize + column]
+//
+//Pro políčko s křížkem/kolečkem vrátí řetězec 'cros'/'circle'
+const getSymbol = (field) => {
+	// Název třídy přizpůsob tvému kódu.
+	if (field.classList.contains('board__field--cross')) {
+		return 'cross'
+	} else if (field.classList.contains('board__field--circle')) {
+		return 'circle'
 	}
 }
 //
